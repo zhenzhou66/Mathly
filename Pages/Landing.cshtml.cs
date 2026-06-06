656fa3c00d3750011521c5db6c5fe60a4571d1b9
zhenzhou66 <zhenzhou0609@gmail.com>
2026-06-06 17:11:45 +0800

    updated header footer

---
+++ Pages/Landing.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mathly.Pages
+{
+    public class LandingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
