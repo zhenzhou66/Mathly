e1d678ea78f32fb8d7ae440333d9ae93c732c03a
Yap Kean Jie <keanjieyap@gmail.com>
2026-07-17 01:12:07 +0800

    Update for student profile

--- Model/StudentInfo.cs
+++ Model/StudentInfo.cs
@@ -10,8 +10,10 @@ namespace Mathly.Models
         public string UserID { get; set; }
         public string StudentName { get; set; }
         public int StudentAge { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string School { get; set; }
+        public DateOnly BirthDate { get; set; }
         public string StudyLevel { get; set; }
         public int ExpPoints { get; set; }
         public DateOnly DateJoined { get; set; }
